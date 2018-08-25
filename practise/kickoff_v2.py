24e61af08a52fabcc6dc1aae1ee5c423e9a3557d
act65 <te1farac@gmail.com>
2018-08-25 13:07:20 +1200

    runs! but doesnt learn much in any reasonable time...

--- practise/kickoff_v2.py
+++ practise/kickoff_v2.py
@@ -2,7 +2,7 @@ import gym
 import gym_soccer
 
 import numpy as np
-import multi_player
+import offline_learner
 
 import tensorflow as tf
 
@@ -11,8 +11,8 @@ import ray
 
 def argumentparser():
     parser = argparse.ArgumentParser(description='Play soccer')
-    parser.add_argument('--max_iters', type=int, default=10000,
-                        help='number of trials')
+    parser.add_argument('--max_iters', type=int, default=1000000,
+                        help='number of steps')
     parser.add_argument('--logdir', type=str, default='/tmp/test/',
                         help='location to save logs')
     parser.add_argument('--task', type=str, default='SoccerEmptyGoal-v0',
@@ -20,10 +20,8 @@ def argumentparser():
     return parser.parse_args()
 
 class GymEnvironment():
-    # seems a little silly, aleady running in a different process...
     def __init__(self, name):
         self.env = gym.make(name)
-        self.env.reset()
 
     def step(self, action):
         action = np.argmax(action[:, :3], axis=1).tolist() + action[0, 3:].tolist()
@@ -41,23 +39,31 @@ class GymEnvironment():
 
 def main(args):
     ray.init()
-    Node = ray.remote(multi_player.Node)
-    player1 = Node.remote(59, 32, logdir=args.logdir+'1')
-    player2 = Node.remote(32, 8, logdir=args.logdir+'2')
-    player3 = Node.remote(32, 32, logdir=args.logdir+'3')
+    Node = ray.remote(offline_learner.ActorCritic)
+    player1 = Node.remote(59, 32, logdir=args.logdir+'/1')
+    player2 = Node.remote(32, 8, logdir=args.logdir+'/2')
+    player3 = Node.remote(64, 64, logdir=args.logdir+'/3')
+    player4 = Node.remote(32, 32, logdir=args.logdir+'/4')
 
     env = GymEnvironment(args.task)
     env.render()
 
-    observation = env.reset(); done = False; reward = 0
-    old_h = [0.0] * 32
+    observation = env.reset(); reward = 0
+    old_o1 = np.zeros([1, 32])
+    old_o3 = np.zeros([1, 64])
+    old_o4 = np.zeros([1, 32])
     for _ in range(args.max_iters):
-        h = player1.call.remote(observation, reward)
-        z = player3.call.remote([a + b for a, b in zip(ray.get(h), old_h)], reward)
-        action = player2.call.remote(z, reward)
-        old_h = ray.get(h)
+        # NB should only have to get the action. the rest can be done elsewhere
+        o1 = ray.get(player1.call.remote(observation, reward))
+        o3 = ray.get(player3.call.remote(np.hstack([o1, old_o4]), reward))
+        action = ray.get(player2.call.remote(o3[:, :32], reward))
+        o4 = ray.get(player4.call.remote(o3[:, 32:], reward))
 
-        observation, reward = env.step(ray.get(action))
+        old_o1 = o1
+        old_o3 = o3
+        old_o4 = o4
+
+        observation, reward = env.step(action)
 
 if __name__ == '__main__':
     main(argumentparser())
